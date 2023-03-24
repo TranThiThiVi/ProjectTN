1b123009ffb611b2e9519a4e42e1ef1625537f3b
Quoc Huy <vodinhquochuy1511@gmail.com>
2023-03-25 00:55:31 +0700

    huyfix

---
+++ app/Mail/SendMailCreateClient.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendMailCreateClient extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+
+    }
+
+    public function build()
+    {
+        return $this->subject('Kích hoạt tài khoản')
+                    ->view('mail.register_mail', [
+                        'data'  =>  $this->data
+                    ]);
+    }
+}
